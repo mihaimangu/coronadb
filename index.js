eb58b72011a2c3c5a5cf95d2dc12b9c6b8f25d4a
mihai mangu <mihai.mangu@gmail.com>
2020-03-20 20:09:50 +0200

    update port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 // const db = require('./database');
 const path = require('path');
 
-var port = 8000;
+var port = 60000;
 
 app.listen(port, function() {
     console.log('app started');
